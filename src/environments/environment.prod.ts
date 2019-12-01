0fe11ee5943b25189839adbfd0837e28b11fa4e8
Christian Bremer <christian.bremer@cstx.de>
2019-12-01 14:30:08 +0100

    Initializes the web app.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  dcConBaseUrl: '${DC_CON_BASE_URL}',
-  viewRoles: ['LOCAL_USER', 'ADMIN'],
-  editRoles: ['ADMIN'],
-  tokenConfig: {
-    allowedUrls: ['${DC_CON_BASE_URL}/api'],
-    issuer: '${TOKEN_ISSUER}',
-    clientId: '${TOKEN_CLIENT_ID}',
-    scope: '${TOKEN_SCOPE}'
-  }
 };
